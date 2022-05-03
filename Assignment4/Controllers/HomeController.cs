05ab45ebaa8510fc6742fc7cee734b0b7fde3dbe
unknown <kujur.rohit95@gmail.com>
2022-05-03 23:04:38 +0530

    Assignment4

---
+++ Assignment4/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Assignment4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Assignment4.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context = new MyDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Accounts);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult CreateAccount(Account a)
+        {
+            context.Accounts.Add(a);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int? accno)
+        {
+            var account_to_edit = (from a in context.Accounts
+                                   where a.AccountNumber == accno
+                                   select a).SingleOrDefault();
+            return View(account_to_edit);
+        }
+        public ActionResult EditAccount(Account a)
+        {
+            context.Entry<Account>(a).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? accno)
+        {
+            var account_to_delete = (from a in context.Accounts
+                                     where a.AccountNumber == accno
+                                     select a).SingleOrDefault();
+            context.Entry<Account>(account_to_delete).State =
+                    System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetNews(string category)
+        {
+            return PartialView(null, category);
+        }
+
+
+
+    }
+}
---
+++ Assignment4/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment4.Models
+{
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public double CurrentBalance { get; set; }
+    }
+}
